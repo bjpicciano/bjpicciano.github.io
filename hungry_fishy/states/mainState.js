7b26df45466b806aa83eaea0a0ade15374a08259
Brian Picciano <BJPicciano@gmail.com>
2016-03-28 17:14:50 -0400

    hungry_fishy made EXACT_FIT scale

--- hungry_fishy/states/mainState.js
+++ hungry_fishy/states/mainState.js
@@ -13,7 +13,7 @@ mainState.prototype = {
             this.scale.pageAlignHorizontally = true;
             this.scale.pageAlignVertically = true;
         } else {
-            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
             this.scale.setMinMax(480, 260, 1024, 768);
             this.scale.pageAlignHorizontally = true;
             this.scale.pageAlignVertically = true;
